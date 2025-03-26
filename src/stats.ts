c396b005dd7c5e320132d972b7a6274efc435f65
taynguyen3110 <taynguyen3110@gmail.com>
2025-03-26 16:12:05 +1100

    fix: change get db stats function to be async

--- src/stats.ts
+++ src/stats.ts
@@ -2,8 +2,8 @@ import { CreateServerOptions, HttpCallAuthentication } from "@jasonai/api";
 import { db_stats } from './services';
 
 export const statsController: CreateServerOptions['beforeRoute'] = (app) => {
-    app.get('/api/statistics', HttpCallAuthentication().middleware(), (req, res) => {
-        const stats = db_stats();
+    app.get('/api/statistics', HttpCallAuthentication().middleware(), async (req, res) => {
+        const stats = await db_stats();
         res.json(stats);
         res.status(200);
     });
